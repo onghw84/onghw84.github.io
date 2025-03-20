89139910cff8044f65bc334bbcfd224997f5b387
onghw84 <onghw84@gmail.com>
2025-03-20 14:30:07 +0800

    update maze algo

--- public/maze.js
+++ public/maze.js
@@ -1,171 +1,196 @@
 var total = 0;
-var level = 0; 
+var level = 10; 
 var focus = '';
 var answer = '';
 
 var start = [99, 118];
-var stop = [101, 102, 103, 104, 105, 106, 107, 119];
+var stop = [101, 102, 103, 104, 105, 106, 107, 149, 150, 151, 152, 153, 154, 155, 119];
 var iconDir = './public/image/memory/';
+var startSrc = iconDir + start[Math.floor(Math.random()*start.length)]+ '.png';
+var endSrc = './public/reward/happybee.gif';
+var dest = -1;
+const r = document.querySelector(':root');
+const dirIndex = ["right","left","up","down"];
+var neighborArray1 = [];
 
-document.getElementById("newGame").addEventListener("click", ()=>{genGame()});
+//document.getElementById("newGame").addEventListener("click", ()=>{genGame()});
 document.getElementById("up").addEventListener("click", moveHandler);
 document.getElementById("down").addEventListener("click", moveHandler);
 document.getElementById("left").addEventListener("click", moveHandler);
 document.getElementById("right").addEventListener("click", moveHandler);
 //todo: add eventlistener for keyboard
+document.onkeypress = function (e) {
+    e = e || window.event;
+	console.log(e.keyCode);
+    // use e.keyCode
+};
 
 document.getElementById("kids").addEventListener("click", function(){
-	level = 0; genGame(); this.style.backgroundColor = "pink";
+	level = 10; reset(); this.style.backgroundColor = "pink";
 })
 document.getElementById("easy").addEventListener("click", function(){
-	level = 1; genGame(); this.style.backgroundColor = "pink";
+	level = 12; reset(); this.style.backgroundColor = "pink";
 })
 document.getElementById("medium").addEventListener("click", function(){
-	level = 2; genGame(); this.style.backgroundColor = "pink";
+	level = 14; reset(); this.style.backgroundColor = "pink";
 })
 document.getElementById("hard").addEventListener("click", function(){
-	level = 3; genGame(); this.style.backgroundColor = "pink";
+	level = 16; reset(); this.style.backgroundColor = "pink";
 })
-
-var text = ""
-for (var i = 0; i < 100; i++) {
-	text += `<div class = "grid top bottom left right" id="${i}"></div>`
-}
-document.getElementById("container1").innerHTML = text;
 
 document.getElementById("total").innerHTML = total;
 let reward = new Reward();
 genGame();
 
-function moveHandler(event){
-	focus = event.currentTarget.myParam;
-	event.currentTarget.style.backgroundColor = "yellowgreen";
-	//check if border exist, move cat
-	
-	
+function moveHandler(event){	
+	var id = event.currentTarget.id;
+	var moveTmp = -1;
+	if (dirIndex.includes(id)){
+		moveTmp = move(focus,dirIndex.indexOf(id));
+	}
+	else {
+		moveTmp = move(focus,neighborArray1[focus].indexOf(parseInt(id)));
+	}
+	if (moveTmp != focus && moveTmp != -1){
+		document.getElementById(focus).innerHTML = '';
+		document.getElementById(moveTmp).innerHTML = `<img id="start" src=${startSrc}></img>`;
+		focus = moveTmp;
+		answerHandler();
+	}
 }
 
-function answerHandler(){    
-  const value = this.innerHTML;
-  if (focus == ""){
-	return;
+async function answerHandler(){    
+  if (focus == dest){
+	document.getElementById("container1").style.backgroundImage = "linear-gradient(180deg,orchid,white)";
+	total += 1; 
+	var text = ""
+	for (var i = 0; i < total; i++){
+		text += `<img src="${iconDir + stop[i]+ '.png'}" style="visibility: visible; width: 70px; height: 70px"></img>`
+	}
+	document.getElementById("container1").innerHTML = `
+		<img src=${endSrc} style="visibility: visible; width: 250px; height: auto"></img>
+		<div class="but" style="flex-wrap: wrap; width: 380px">${text}</div>`;
+	document.getElementById("total").innerHTML = total;
+	reward.showReward(total, 1);
+	await new Promise(resolve => setTimeout(resolve, 2000));
+	genGame();
   }
-  else {
-	  document.getElementById(focus).innerHTML = value;
-	  
-	  const index = grid.indexOf(focus);
-	  if (answer[index] == value){
-		document.getElementById(focus).style.color = "blue";
-		correct += 1;
-		if (correct == emptyGrid.length){	//all correct
-			document.getElementById("container1").style.backgroundColor = "pink";
-			total += 1;
-			document.getElementById("total").innerHTML = total;
-			reward.showReward(total, 1);			
-		}
-	  }
-	  return;
-  }
-}
-
-function genPath(){
-	
 }
 
 function genGame(){	
-
-	var startIndex = 0;
-	var stopIndex = Math.ceil(Math.random()*100);
-	var startSrc = iconDir + start[Math.floor(Math.random()*start.length)]+ '.png';
-	var stopSrc = iconDir + stop[Math.floor(Math.random()*stop.length)]+ '.png';
+	var stopSrc = iconDir + stop[total]+ '.png';
+	document.getElementById("container1").style.backgroundImage = "";
+	document.getElementById("container1").style.backgroundColor = "white";
+	const size = Math.floor((380-10)/level)-2;
+	r.style.setProperty('--size', size+'px');
+	
+	var text = ""; var neighborArray = Array(level**2);
+	for (var i = 0; i < level**2; i++) {
+		text += `<div class = "grid top bottom left right" id="${i}"></div>`;
+		neighborArray[i] = [];
+		if (i%level != level-1){
+			neighborArray[i].push(i+1);
+		}else{neighborArray[i].push(-1);}
+		if (i%level != 0){
+			neighborArray[i].push(i-1);
+		}else{neighborArray[i].push(-1);}		
+		if (i >= level){
+			neighborArray[i].push(i-level);
+		}else{neighborArray[i].push(-1);}	
+		if (i < level*(level-1)){
+			neighborArray[i].push(i+level);
+		}else{neighborArray[i].push(-1);}
+	}
+	neighborArray1 = [...neighborArray];
+	document.getElementById("container1").innerHTML = text;
+	for (var i = 0; i < level**2; i++) {
+		document.getElementById(i).addEventListener("click", moveHandler);
+	}
+	
+	var startIndex = Math.floor(Math.random()*level**2);
 	var borderClass = ["right","left","top","bottom"];
 	var borderClass1 = ["left","right","bottom","top"];
 	
 	document.getElementById(startIndex).innerHTML = `<img id="start" src=${startSrc}></img>`;
-	document.getElementById(stopIndex).innerHTML = `<img id="stop" src=${stopSrc}></img>`;
+	focus = startIndex;
 
-	//generate path from start to stop
-	console.log(stopIndex);
-	var path = [startIndex]; var step = 0; var count = 0;
-	while (path[step] != stopIndex){
-		count += 1;
-		var moveType = Math.floor(Math.random()*4);
-		var moveTmp = move(path[step],moveType);
-		if (path.indexOf(moveTmp) == -1){			
-			document.getElementById(path[step]).classList.remove(borderClass[moveType]);
+	//generate path
+	var visited = [startIndex];
+	var path = [startIndex]; var step = 0; var placeEnd = false;
+	while (visited.length != level**2){
+		if (!placeEnd && step == level*level/2){
+		//if (!placeEnd && step == 2){
+			document.getElementById(path[step]).innerHTML = `<img id="stop" src=${stopSrc}></img>`;
+			dest = path[step];
+			placeEnd = true;
+		}
+		
+		var neighbor = neighborArray[path[step]]; var moveArray = [];
+
+		for (var j = 0; j < 4; j++){
+			if (neighbor[j] != -1 && visited.indexOf(neighbor[j])==-1){
+				moveArray.push(j);				
+			}
+		}
+
+		if (moveArray.length > 0){
+			var moveType = Math.floor(Math.random()*moveArray.length);
+			var moveTmp = neighbor[moveArray[moveType]];
+			document.getElementById(path[step]).classList.remove(borderClass[moveArray[moveType]]);
 			path.push(moveTmp);
-			step += 1;
-			document.getElementById(path[step]).classList.remove(borderClass1[moveType]);
-		}
-		console.log(path);
-		if (count > 100){
-			path = [path[Math.floor(Math.random()*path.length)]];
-			count = 0; step = 0;
-		}
-	}
-	
-	//this.classList.remove("close");
-	emptyGrid = []; var emptyIndex = 0;
-	for (var i = 0; i < (level*2+1)*5; i++){
-		emptyIndex = (emptyIndex + Math.ceil(Math.random()*10))%grid.length;
-		emptyGrid.push(grid[emptyIndex]);
-	}
-	emptyGrid = removeDuplicates(emptyGrid);
-
-	//fill in grid
-	for (var i = 0; i < grid.length; i++) {
-		if (emptyGrid.includes(grid[i])){
-			document.getElementById(grid[i]).style.fontWeight = "normal";
-			document.getElementById(grid[i]).style.fontStyle = "italic";
-			document.getElementById(grid[i]).addEventListener("click", moveHandler, false);
-			document.getElementById(grid[i]).myParam = grid[i];
-			document.getElementById(grid[i]).innerHTML = "";
+			step += 1; visited.push(moveTmp);
+			document.getElementById(path[step]).classList.remove(borderClass1[moveArray[moveType]]);
 		}
 		else {
-			document.getElementById(grid[i]).removeEventListener("click", moveHandler);
-			document.getElementById(grid[i]).style.fontWeight = "bold";
-			document.getElementById(grid[i]).style.fontStyle = "normal";
-			document.getElementById(grid[i]).innerHTML = answer[i];
+			path.pop(); step -= 1;
 		}
 	}
 }
 
 function move(index, direction){
-	if (direction == 0){	//Left
-		if (index%10 == 9){
+	if (direction == 0){	//move right
+		if (index%level == level-1 || document.getElementById(index).classList.value.includes('right')){
 			return index;
 		}
 		else {
 			return index + 1;
 		}
 	}
-	else if (direction == 1){	//Right
-		if (index%10 == 0){
+	else if (direction == 1){	//move left
+		if (index%level == 0 || document.getElementById(index).classList.value.includes('left')){
 			return index;
 		}
 		else {
 			return index - 1;
 		}
 	}
-	else if (direction == 2){	//Up
-		if (index <= 9){
+	else if (direction == 2){	//move up
+		if (index <= level-1 || document.getElementById(index).classList.value.includes('top')){
 			return index;
 		}
 		else {
-			return index - 10;
+			return index - level;
 		}		
 	}
-	else if (direction == 3){	//Down
-		if (index >= 90){
+	else if (direction == 3){	//move down
+		if (index >= level*(level-1) || document.getElementById(index).classList.value.includes('bottom')){
 			return index;
 		}
 		else {
-			return index + 10;
+			return index + level;
 		}		
 	}
 	else {
 		return index;
 	}
+}
+
+function reset(){
+	genGame();
+	document.getElementById("kids").style.backgroundColor = "white";
+	document.getElementById("easy").style.backgroundColor = "white";
+	document.getElementById("medium").style.backgroundColor = "white";
+	document.getElementById("hard").style.backgroundColor = "white";
 }
 
 document.getElementById("song").addEventListener("click", function (){
