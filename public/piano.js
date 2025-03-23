0d4c93feba04b1a8e6f0431fdbc38315a191e097
onghw84 <onghw84@gmail.com>
2025-03-23 19:37:04 +0800

    update piano

--- public/piano.js
+++ public/piano.js
@@ -6,12 +6,19 @@ var keyArray = Array(keys.length);
 
 for (var i = 0; i < keys.length; i++){	
 	keyArray[i] = keys[i].getAttribute("data-note");
+	music[i] = new Audio(audio_dir+keyArray[i]+'.mp3');
 	keys[i].addEventListener("mousedown", function(e){		
 		playSound(e.currentTarget);
 	});
 	keys[i].addEventListener("mouseup", function(e){
 		stopSound(e.currentTarget);
 	});
+	keys[i].addEventListener("touchstart", function(e){		
+		playSound(e.currentTarget);
+	});
+	keys[i].addEventListener("touchend", function(e){
+		stopSound(e.currentTarget);
+	});	
 }
 
 console.log(keyArray);
@@ -19,13 +26,13 @@ console.log(keyArray);
 
 function playSound(item){	
   const note = item.getAttribute("data-note");
-  const path = audio_dir+note+'.mp3';  
-  music[keyArray.indexOf(note)] = new Audio(path);
+  music[keyArray.indexOf(note)].currentTime = 0;
   music[keyArray.indexOf(note)].play();
 }
 
 async function stopSound(item){	
   await new Promise(resolve => setTimeout(resolve, 200));
   const note = item.getAttribute("data-note");
+  music[keyArray.indexOf(note)].pause();
   music[keyArray.indexOf(note)].pause();
 }
