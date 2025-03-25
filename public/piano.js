d9dd77729daa218da69e068e0b8622759fe5a5e2
onghw84 <onghw84@gmail.com>
2025-03-25 15:20:18 +0800

    update piano.js

--- public/piano.js
+++ public/piano.js
@@ -6,28 +6,28 @@ var sheet = document.getElementById("sheet");
 var playing = Array(keys.length).fill(0);
 var count = 0;
 var selectedSong = 0;
-const sheetLen = 15;
+const sheetLen = 8;
 const altNames = {'A#0': 'Bb0','C#1': 'Db1','D#1': 'Eb1','F#1': 'Gb1','G#1': 'Ab1','A#1': 'Bb1','C#2': 'Db2','D#2': 'Eb2','F#2': 'Gb2','G#2': 'Ab2','A#2': 'Bb2','C#3': 'Db3',
   'D#3': 'Eb3','F#3': 'Gb3','G#3': 'Ab3','A#3': 'Bb3','C#4': 'Db4','D#4': 'Eb4','F#4': 'Gb4','G#4': 'Ab4','A#4': 'Bb4','C#5': 'Db5','D#5': 'Eb5','F#5': 'Gb5','G#5': 'Ab5',
   'A#5': 'Bb5','C#6': 'Db6','D#6': 'Eb6','F#6': 'Gb6','G#6': 'Ab6','A#6': 'Bb6','C#7': 'Db7','D#7': 'Eb7','F#7': 'Gb7','G#7': 'Ab7','A#7': 'Bb7'};
 
 const sheets = [
-["C2", "C2", "G2", "G2", "A2", "A2", "G2", "F2", "F2", "E2", "E2", "D2", "D2", "C2", "G2", "G2", "F2", "F2", "E2", "E2", "D2", "G2", "G2", "F2", "F2",
- "E2", "E2", "D2", "C2", "C2", "G2", "G2", "A2", "A2", "G2", "F2", "F2", "E2", "E2", "D2", "D2", "C2"],
-["G2","E2","E2","F2","D2","D2","C2","D2","E2","F2","G2","G2","G2","G2","E2","E2","F2","D2","D2","C2","E2","G2","G2","E2","D2","D2","D2","D2","D2","E2","F2",
-"E2","E2","E2","E2","E2","F2","G2","G2","E2","E2","F2","D2","D2","C2","E2","G2","G2","C2"],
-["C2", "D2", "F2", "G2", "A2", "A2", "G2", "F2", "D2", "F2", "F2", "D2", "F2", "D2", "F2", "F2", "D2", "C2", "C3", "A2", "A2", "F2", "G2", "G2", "F2"],
-["C2", "C2", "D2", "C2", "F2", "E2", "C2", "C2", "D2", "C2", "G2", "F2", "C2", "C2", "C3", "A2", "F2", "E2", "D2", "Bb2", "Bb2", "A2", "F2", "G2", "F2"],
-["E2", "D2", "C2", "D2", "E2", "E2", "E2", "D2", "D2", "D2", "E2", "E2", "E2", "E2", "D2", "C2", "D2", "E2", "E2", "E2", "E2", "D2", "D2", "E2", "D2", "C2"],
-["D2", "F2", "D3", "C3", "Bb2", "D2", "F2", "Bb2", "A2", "Eb2", "F2", "Eb3", "D3", "Bb2", "C3", "Bb2", "G2", "F2", "D2", "F2", "D3", "C3", "Bb2", "D2", "F2", "Bb2", "A2", "G2", "F2", "Bb2", "Eb3", "D3", "Bb2", "C3", "G2", "A2", "Bb2"],
-["G2", "E2", "G2", "G2", "E2", "G2", "A2", "G2", "F2", "E2", "D2", "E2", "F2", "E2", "F2", "G2", "C2", "C2", "C2", "C2", "D2", "E2", "F2", "G2", "G2", "D2", "D2", "F2", "E2", "D2", "C2"],
-["D2", "E2", "G2", "A2", "B2", "A2", "G2", "F#2", "E2", "D2", "F#2", "E2", "F#2", "A2", "B2", "D3", "D3", "B2", "A2", "F#2", "E2", "D2", "E2", "F#2", "A2", "F#2", "A2", "B2", "F#2", "A2", "F#2", "E2", "D2", "D2", "D3", "B2", "A2", "G2", "F#2", "E2", "D2"],
-["G2", "Bb1", "G2", "Bb1", "Bb1", "Ab2", "G2", "F2", "F2", "Bb1", "F2", "Bb1", "Bb1", "G2", "Ab2", "Bb2", "G2", "Bb2", "Bb2", "G2", "F2", "Ab2", "F2", "Ab2", "Ab2", "Ab2", "Ab2", "Ab2", "G2", "F2", "Eb2"],
-["F2", "F2", "F2", "C2", "D2", "D2", "C2", "A2", "A2", "G2", "G2", "F2", "C2", "F2", "F2", "F2", "C2", "D2", "D2", "C2", "A2", "A2", "G2", "G2", "F2", "C2", "C2", "F2", "F2", "F2", "C2", "C2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "F2", "C2", "D2", "D2", "C2", "A2", "A2", "G2", "G2", "F2"],
-["C2", "F2", "F2", "G2", "F2", "E2", "D2", "D2", "D2", "G2", "G2", "A2", "G2", "F2", "E2", "C2", "C2", "A2", "A2", "Bb2", "A2", "G2", "F2", "D2", "C2", "D2", "G2", "E2", "F2", 
-"C2", "F2", "F2", "F2", "E2", "E2", "F2", "E2", "D2", "C2", "G2", "A2", "G2", "G2", "F2", "F2", "C3", "C2", "C2", "C2", "D2", "G2", "E2", "F2"],
-["C2", "F2", "C2", "A1", "Bb1", "C2", "C2", "C2", "D2", "C2", "Bb1", "A1", "G1", "F2", "F2", "F2", "D2", "F2", "E2", "G2", "G2", "F2"],
-["G1", "C2", "C2", "C2", "C2", "E2", "G2", "E2", "C2", "D2", "B1", "G1", "G2", "E2", "C2", "G1", "C2", "C2", "C2", "C2", "E2", "G2", "E2", "C2", "D2", "G1", "G1", "C2"]
+["C2/1", "C2/1", "G2/1", "G2/1", "A2/1", "A2/1", "G2/2", "F2/1", "F2/1", "E2/1", "E2/1", "D2/1", "D2/1", "C2/2", "G2/1", "G2/1", "F2/1", "F2/1", "E2/1", "E2/1", "D2/2", "G2/1", "G2/1", "F2/1", "F2/1",
+ "E2/1", "E2/1", "D2/2", "C2/1", "C2/1", "G2/1", "G2/1", "A2/1", "A2/1", "G2/2", "F2/1", "F2/1", "E2/1", "E2/1", "D2/1", "D2/1", "C2/2"],
+["G2/1","E2/1","E2/2","F2/1","D2/1","D2/2","C2/1","D2/1","E2/1","F2/1","G2/1","G2/1","G2/2","G2/1","E2/1","E2/2","F2/1","D2/1","D2/2","C2/1","E2/1","G2/1","G2/1","E2/4","D2/1","D2/1","D2/1","D2/1","D2/1","E2/1","F2/2",
+"E2/1","E2/1","E2/1","E2/1","E2/1","F2/1","G2/2","G2/1","E2/1","E2/2","F2/1","D2/1","D2/2","C2/1","E2/1","G2/1","G2/1","C2/4"],
+["C2/2", "D2/2", "F2/2", "G2/2", "A2/2", "A2/1", "G2/1", "F2/4", "D2/2", "F2/2", "F2/1", "D2/1", "F2/1", "D2/1", "F2/2", "F2/1", "D2/1", "C2/4", "C3/2", "A2/2", "A2/1", "F2/1", "G2/1", "G2/1", "F2/4"],
+["C2/1", "C2/1", "D2/2", "C2/2", "F2/2", "E2/4", "C2/1", "C2/1", "D2/2", "C2/2", "G2/2", "F2/4", "C2/1", "C2/1", "C3/2", "A2/2", "F2/2", "E2/2", "D2/4", "Bb2/1", "Bb2/1", "A2/2", "F2/2", "G2/2", "F2/4"],
+["E2/1", "D2/1", "C2/1", "D2/1", "E2/1", "E2/1", "E2/2", "D2/1", "D2/1", "D2/2", "E2/1", "E2/1", "E2/2", "E2/1", "D2/1", "C2/1", "D2/1", "E2/1", "E2/1", "E2/1", "E2/1", "D2/1", "D2/1", "E2/1", "D2/1", "C2/4"],
+["D2/3", "F2/1", "D3/2", "C3/5", "Bb2/1", "D2/3", "F2/1", "Bb2/2", "A2/6", "Eb2/3", "F2/1", "Eb3/2", "D3/5", "Bb2/1", "C3/2", "Bb2/2", "G2/2", "F2/6", "D2/3", "F2/1", "D3/2", "C3/5", "Bb2/1", "D2/3", "F2/1", "Bb2/2", "A2/6", "G2/1", "F2/3", "Bb2/1", "Eb3/2", "D3/5", "Bb2/2", "C3/2", "G2/2", "A2/2", "Bb2/6"],
+["G2/2", "E2/2", "G2/4", "G2/2", "E2/2", "G2/4", "A2/2", "G2/2", "F2/2", "E2/2", "D2/2", "E2/2", "F2/2", "E2/1", "F2/1", "G2/2", "C2/2", "C2/2", "C2/2", "C2/1", "D2/1", "E2/1", "F2/1", "G2/4", "G2/2", "D2/2", "D2/2", "F2/2", "E2/2", "D2/2", "C2/2"],
+["D2/1", "E2/1", "G2/1", "A2/1", "B2/4", "A2/1", "G2/1", "F#2/1", "E2/1", "D2/4", "F#2/1", "E2/1", "F#2/1", "A2/1", "B2/1", "D3/1", "D3/1", "B2/1", "A2/1", "F#2/1", "E2/1", "D2/1", "E2/4", "F#2/1", "A2/1", "F#2/1", "A2/1", "B2/4", "F#2/1", "A2/1", "F#2/1", "E2/1", "D2/4", "D2/2", "D3/1", "B2/1", "A2/1", "G2/1", "F#2/1", "E2/1", "D2/4"],
+["G2/3", "Bb1/1", "G2/3", "Bb1/1", "Bb1/1", "Ab2/2", "G2/2", "F2/4", "F2/3", "Bb1/1", "F2/3", "Bb1/1", "Bb1/1", "G2/2", "Ab2/2", "Bb2/4", "G2/2", "Bb2/2", "Bb2/2", "G2/2", "F2/2", "Ab2/2", "F2/4", "Ab2/1", "Ab2/1", "Ab2/1", "Ab2/1", "Ab2/2", "G2/1", "F2/1", "Eb2/4"],
+["F2/1", "F2/1", "F2/1", "C2/1", "D2/1", "D2/1", "C2/2", "A2/1", "A2/1", "G2/1", "G2/1", "F2/3", "C2/1", "F2/1", "F2/1", "F2/1", "C2/1", "D2/1", "D2/1", "C2/2", "A2/1", "A2/1", "G2/1", "G2/1", "F2/4", "C2/1", "C2/1", "F2/1", "F2/1", "F2/1", "C2/1", "C2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "F2/1", "C2/1", "D2/1", "D2/1", "C2/2", "A2/1", "A2/1", "G2/1", "G2/1", "F2/4"],
+["C2/2", "F2/2", "F2/1", "G2/1", "F2/1", "E2/1", "D2/2", "D2/2", "D2/2", "G2/2", "G2/1", "A2/1", "G2/1", "F2/1", "E2/2", "C2/2", "C2/2", "A2/2", "A2/1", "Bb2/1", "A2/1", "G2/1", "F2/2", "D2/2", "C2/2", "D2/2", "G2/2", "E2/2", "F2/4", 
+"C2/2", "F2/2", "F2/2", "F2/2", "E2/4", "E2/2", "F2/2", "E2/2", "D2/2", "C2/4", "G2/2", "A2/2", "G2/1", "G2/1", "F2/1", "F2/1", "C3/2", "C2/2", "C2/1", "C2/1", "D2/2", "G2/2", "E2/2", "F2/4"],
+["C2/2", "F2/2", "C2/2", "A1/3", "Bb1/1", "C2/6", "C2/1", "C2/1", "D2/2", "C2/2", "Bb1/3", "A1/1", "G1/8", "F2/2", "F2/2", "F2/4", "D2/3", "F2/1", "E2/3", "G2/1", "G2/2", "F2/2"],
+["G1/2", "C2/2", "C2/1", "C2/1", "C2/2", "E2/2", "G2/2", "E2/2", "C2/4", "D2/2", "B1/2", "G1/4", "G2/2", "E2/2", "C2/4", "G1/2", "C2/2", "C2/1", "C2/1", "C2/2", "E2/2", "G2/2", "E2/2", "C2/4", "D2/4", "G1/2", "G1/2", "C2/4"]
 ]
 
 const sheetsName = ["ABC","Bee","Angel","Birthday","Little Lamb","Lullaby","I love you","Lamp","Deepavali","O' Macdonald","Xmas NewYear","HippieVan1","Wheel on the bus"];
@@ -43,7 +43,7 @@ selSong.onchange = function() {
 	if (selSong.value != -1){
 		reset();
 		selectedSheet1 = sheets[selSong.value];
-		selectedSheet = sheets[selSong.value].map((el) => {return el.includes('#')? altNames[el]:el})
+		selectedSheet = sheets[selSong.value].map((el) => {return el.includes('#')? altNames[el.substr(0, el.length - 2)]:el.substr(0, el.length - 2)})
 		
 		document.querySelectorAll(`[data-note='${selectedSheet[0]}']`)[0].classList.add("highlight1");
 		currentKey = selectedSheet[0];
@@ -57,7 +57,8 @@ selSong.onchange = function() {
 function genSheet(start){
 	var text = '';
 	for (var i = start; i < Math.min(selectedSheet1.length, start+sheetLen); i++){
-		text += `<span class="sheet-note" data-sheetNote=${i}>${selectedSheet1[i].substr(0, selectedSheet1[i].length - 1).replace('b','♭')}</span>`;
+		var pulse = parseInt(selectedSheet1[i][selectedSheet1[i].length-1]);
+		text += `<span class="sheet-note" data-sheetNote=${i} style="padding-right: ${pulse*15}px; background-size: 200% 100%; animation-duration: ${pulse*0.3}s;">${selectedSheet1[i].substr(0, selectedSheet1[i].length - 3).replace('b','♭')}</span>`;
 	}
 	sheet.innerHTML = text;	
 	document.querySelectorAll(`[data-sheetNote='${start}']`)[0].classList.add("highlight");
@@ -90,10 +91,25 @@ function playSound(item){
   music[keyArray.indexOf(note)].currentTime = 0;
   music[keyArray.indexOf(note)].play();
   playing[keyArray.indexOf(note)] = 1;
-  //if (currentKey.indexOf(note)!= -1){
+  if (currentKey == note){
+	document.querySelectorAll(`[data-sheetNote='${count}']`)[0].classList.add("highlightSlide");
+	console.log("animate");
+  }
+}
+
+async function celebrate(){
+	document.getElementById("bee").style.visibility = "visible";
+	sheet.innerHTML = "";
+	selSong.value = -1;
+	await new Promise(resolve => setTimeout(resolve, 3000));
+	document.getElementById("bee").style.visibility = "hidden";
+}
+
+async function stopSound(item){	
+  const note = item.getAttribute("data-note");
   if (currentKey == note){
 	  count +=1;
-	  document.querySelectorAll(`[data-sheetNote='${count-1}']`)[0].classList.remove("highlight");	  
+	  document.querySelectorAll(`[data-sheetNote='${count-1}']`)[0].classList.remove("highlight");
 	  document.querySelectorAll(`[data-note='${selectedSheet[count-1]}']`)[0].classList.remove("highlight1");
 	  if (count < selectedSheet.length){
 		document.querySelectorAll(`[data-note='${selectedSheet[count]}']`)[0].classList.add("highlight1");
@@ -110,22 +126,10 @@ function playSound(item){
 	  if (count == selectedSheet.length){
 		  celebrate();
 	  }
-  }
-}
-
-async function celebrate(){
-	document.getElementById("bee").style.visibility = "visible";
-	sheet.innerHTML = "";
-	selSong.value = -1;
-	await new Promise(resolve => setTimeout(resolve, 3000));
-	document.getElementById("bee").style.visibility = "hidden";
-}
-
-async function stopSound(item){	
-  const note = item.getAttribute("data-note");
+  }  
   playing[keyArray.indexOf(note)] = 0;
-  await new Promise(resolve => setTimeout(resolve, 500));
-  if (playing[keyArray.indexOf(note)] == 0 && music[keyArray.indexOf(note)].currentTime > 0.3){
+  await new Promise(resolve => setTimeout(resolve, 300));
+  if (playing[keyArray.indexOf(note)] == 0 && music[keyArray.indexOf(note)].currentTime > 0.2){
 	music[keyArray.indexOf(note)].pause();
   }
 }
